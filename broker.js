7573ff23f4b030f17f24bea7d0060d7b1052fbdf
MJKubas <67106309+MJKubas@users.noreply.github.com>
2021-06-10 16:39:06 +0200

    Update broker.js

--- broker.js
+++ broker.js
@@ -1,15 +1,17 @@
 // Mosca MQTT broker
 var mosca = require('mosca')
-const crypto = require('crypto');
-const bp = require('body-parser');
-var express = require("express");
-var mysql = require('mysql');
-
 var settings = {port: 1235};
+const bp = require('body-parser');
 var broker = new mosca.Server(settings);
+var Gpio = require('onoff').Gpio;
+var buzzer = new Gpio(4, 'out');
 
+
+var express = require("express");
 var app = express();
 
+var mysql = require('mysql');
+const e = require('express');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -42,9 +44,10 @@ con.query("SELECT * FROM Devices", function (err, result, fields) {
     })
 });
 
-app.post("/register", function(req, res){
+app.post("/register", async(req, res) => {
     var body = req.body;
     var toAdd = true;
+    console.log(body);
     if(body.AuthToken == "TEST"){ //Change auth value
         if(body.MqttType == "Publisher"){
             if(jsonConnectedDevices[connectedDevices]){
@@ -66,7 +69,7 @@ app.post("/register", function(req, res){
     		        if (err) throw err;
     		        console.log("Table created");
 		        });
-                var sqlValueClear = `CREATE EVENT cleaning ON SCHEDULE EVERY 1 MONTH ENABLE DO DELETE FROM ${body.Device.Topic} WHERE 'timeStamp' < CURRENT_TIMESTAMP - INTERVAL 6 MONTH;`
+                var sqlValueClear = `CREATE EVENT cleaning_${body.Device.Topic} ON SCHEDULE EVERY 1 MONTH ENABLE DO DELETE FROM ${body.Device.Topic} WHERE 'timeStamp' < CURRENT_TIMESTAMP - INTERVAL 6 MONTH;`
                 con.query(sqlValueClear, function (err, result) {
     		        if (err) throw err;
     		        console.log("Delete event set");
@@ -75,6 +78,9 @@ app.post("/register", function(req, res){
             res.send("Done")
         }
         else if(body.MqttType == "Viewer"){
+	    jsonConnectedDevices[connectedDevices].length = 0
+
+	    await getDevices();
             var test = JSON.stringify(jsonConnectedDevices)
             console.log(test)
             res.send(test)            
@@ -93,6 +99,12 @@ app.get("/", function(req, res){
     })
 });
 
+app.get("/data", async(req, res) => {
+    var result = await getData(req.query.topic);
+    console.log(result);
+    res.json(result);
+});
+
 app.listen(8080, () => {
  console.log("Server running on port 8080");
 });
@@ -103,36 +115,111 @@ broker.on('ready', ()=>{
 })
 
 broker.on('published', (packet)=>{
+    var toAdd = true;
     var message = packet.payload.toString()
     var topic = packet.topic.toString()
-    if(jsonConnectedDevices[connectedDevices]){
-        jsonConnectedDevices[connectedDevices].forEach(element => {
-            if(element.Topic == topic){
-                var sql = `INSERT INTO ${topic} (timeStamp, value) VALUES (CURRENT_TIMESTAMP, '${message}')`
-                con.query(sql, function (err, result) {
-                    if (err) throw err;
-                    console.log("1 record inserted");
-                });
-                console.log(topic)
-                console.log(message)
-            }
-        });
+    if(topic == "alert"){
+        if(message=="on" && buzzer.readSync() == 0){
+            buzzer.writeSync(1);
+        }
+        else if(message == "off" && buzzer.readSync() == 1){
+            buzzer.writeSync(0);
+        }
     }
-    console.log(broker.clients.toString())
+    else{
+        var parsed
+        try {
+            parsed = JSON.parse(message);
+        } catch (e) {
+            return console.log("error " + message);
+        }
+
+        if(parsed.AuthToken == "TEST"){
+            if(parsed.MqttType == "Publisher"){
+                if(jsonConnectedDevices[connectedDevices]){
+                    jsonConnectedDevices[connectedDevices].forEach(element => {
+                        if(element.Topic == topic){
+                            toAdd = false
+                        }
+                    });                
+                }
+                if(toAdd){
+                    jsonConnectedDevices[connectedDevices].push(parsed.Device)
+                    var sqlDeviceAdd = `INSERT INTO Devices (Type, Topic, View) VALUES ('${parsed.Device.Type}', '${topic}', '${parsed.Device.View}')`;
+                    con.query(sqlDeviceAdd, function (err, result) {
+                        if (err) throw err;
+                        console.log("Table created");
+                    });
+                    var sqlValueAdd = `CREATE TABLE IF NOT EXISTS ${topic} (id INT AUTO_INCREMENT PRIMARY KEY, timeStamp VARCHAR(255), value VARCHAR(255))`;
+                    con.query(sqlValueAdd, function (err, result) {
+                        if (err) throw err;
+                        console.log("Table created");
+                    });
+                    var sqlValueClear = `CREATE EVENT cleaning_${topic} ON SCHEDULE EVERY 1 MONTH ENABLE DO DELETE FROM ${topic} WHERE 'timeStamp' < CURRENT_TIMESTAMP - INTERVAL 6 MONTH;`
+                    con.query(sqlValueClear, function (err, result) {
+                        if (err) throw err;
+                        console.log("Delete event set");
+                    });
+                }
+
+                //jsonConnectedDevices[connectedDevices].forEach(element => {
+                //    if(element.Topic == topic){
+                        var sql = `INSERT INTO ${topic} (timeStamp, value) VALUES (CURRENT_TIMESTAMP, '${parsed.Value}')`
+                        con.query(sql, function (err, result) {
+                            if (err) throw err;
+                            console.log("1 record inserted");
+                        });
+                        console.log(topic)
+                        console.log(message)
+                  //  }
+                //});            
+            }        
+        }        
+    }
 })
 
-function Authentication(input){
-    const password = 'Password123';
-    const storedHashString = 'J9IBFSw0U1EFsH/ysL+wak6wb8s=';
-    const storedSaltString = '2nX0MZPZlwiW8bYLlVrfjBYLBKM=';
+queryDevices = () =>{
+    return new Promise((resolve, reject) =>{
+	con.query("SELECT * FROM Devices", function (err, result, fields) {
+    	    if (err) return reject(err);
+	    return resolve(result)
+	});
+    });
+};
 
-    const storedHashBytes = new Buffer.from(storedHashString, 'base64');
-    const storedSaltBytes = new Buffer.from(storedSaltString, 'base64');
+async function getDevices(){
+    try{
+	const devices = await queryDevices();
+	devices.forEach(element => {
+            device = {
+            	Type: element.Type,
+            	Topic: element.Topic,
+            	View: element.View
+            };
+            jsonConnectedDevices[connectedDevices].push(device);
+    	})
+    } catch(error){
+	console.log(error)
+    }
+    console.log(jsonConnectedDevices[connectedDevices])
+}
 
-    crypto.pbkdf2(password, storedSaltBytes, 1000, 20, 'sha1',
-        (err, calculatedHashBytes) => {
-            const correct = calculatedHashBytes.equals(storedHashBytes);
-            console.log('Password is ' + (correct ? 'correct 😎' : 'incorrect 😭'));
-        }
-    );
+queryData = (topic) => {
+    return new Promise((resolve, reject) =>{
+	var sqlGetData = `SELECT date(timeStamp) as day, avg(value) as avgValue FROM ${topic} WHERE timeStamp BETWEEN CURRENT_TIMESTAMP - INTERVAL 1 MONTH AND CURRENT_TIMESTAMP GROUP BY date(timeStamp)`;
+	con.query(sqlGetData, function (err, result, fields) {
+    	    if (err) return reject(err);
+	    return resolve(result)
+	});
+    });
+};
+
+async function getData(topic){
+    var data;
+    try{
+	data = await queryData(topic);
+    } catch(error){
+	console.log(error)
+    }
+    return data;
 }
